6c2966a66cec5507688bf8e86339b83b5f1ddf21
Aleksandre Maisuradze <aleksandremaisuradze11@gmail.com>
2025-02-18 19:17:45 +0400

    corrections after push in github

--- src/app/components/navigation/Navigation.js
+++ src/app/components/navigation/Navigation.js
@@ -4,7 +4,6 @@ import { routes } from "../../constants/routes";
 import './Navigation.css';
 import './NavigationResp.css';
 import Image from "next/image";
-import Bars from '../../images/bars.svg';
 import { useState } from 'react';
 
 export default function Navigation() {
@@ -23,7 +22,7 @@ export default function Navigation() {
                 <Link href={routes.installation} className="linkP" rel="preload" >ინსტალაცია</Link>
                 <Link href={routes.contact} className="linkP linkL" rel="preload" >კონტაქტი</Link>
             </div>
-            <Image src={Bars} width={24} height={24} alt='bars' className='bars' onClick={toggleMenu} /> 
+            <Image src='/images/bars.svg' width={24} height={24} alt='bars' className='bars' onClick={toggleMenu} /> 
         </>
     );
 }
