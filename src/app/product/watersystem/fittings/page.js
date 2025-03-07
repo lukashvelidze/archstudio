cedb6af082913b8c9da5d7bee8b97789e8cb1a76
Aleksandre Maisuradze <aleksandremaisuradze11@gmail.com>
2025-03-07 11:32:24 +0400

    fix render key bag

--- src/app/product/watersystem/fittings/page.js
+++ src/app/product/watersystem/fittings/page.js
@@ -3,6 +3,7 @@ import { fittingsJSON } from '@/app/constants/fittingsJSON'
 import Link from 'next/link'
 import '../watersystem.css'
 import React from 'react'
+import { waterSystemProducts } from '@/app/constants/productJSON'
 
 export default function Fittings() {
 
@@ -12,8 +13,8 @@ export default function Fittings() {
         <div className='main_div' >
             {/* <h1>Water Systems</h1> */}
             <div className='list_div' >
-                {fittingsJSON.map((product) => (
-                    <Link href={`/product/watersystem/fittings/${product.id}`} rel='preload' >
+                {fittingsJSON.map((product, index) => (
+                    <Link href={`/product/watersystem/fittings/${product.id}`} rel='preload'  key={index}  >
                         <div className='list_product' >
                             {product.name}
                         </div>
--- src/app/product/watersystem/page.js
+++ src/app/product/watersystem/page.js
@@ -6,9 +6,9 @@ export default function WaterSystems() {
   return (
     <div className='main_div' >
       {/* <h1>Water Systems</h1> */}
-      <div className='list_div' key={waterSystemProducts.id} >
-        {waterSystemProducts.map((product) => (
-          <Link href={`/product/watersystem/${product.id}`} rel='preload' >
+      <div className='list_div'  >
+        {waterSystemProducts.map((product, index) => (
+          <Link href={`/product/watersystem/${product.id}`} rel='preload' key={index}   >
             <div className='list_product' >
               {product.name}
             </div>
