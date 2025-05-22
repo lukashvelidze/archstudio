4ae5a65b0ad66a6ddfc7f946c39cab035163958c
lukashvelidze <lukashvelidze@mail.weber.edu>
2025-05-22 16:42:39 +0400

    pages support

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,14 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Required if you're using <Image />
+  },
+  basePath: '/your-repo-name', // Important for routing!
+};
+
+module.exports = nextConfig;
